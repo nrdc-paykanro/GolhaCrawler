0f3fa6ccab471193b8d0c159725d8af7c15425af
nrdc-paykanro <112057043+nrdc-paykanro@users.noreply.github.com>
2022-08-28 15:12:01 +0430

    puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
-import fs from 'fs';
-import path from 'path';
-import axios from 'axios';
-import cheerio from 'cheerio';
+import fs from "fs";
+import path from "path";
+import axios from "axios";
+import cheerio from "cheerio";
+import puppeteer from "puppeteer"
+//extract File address from pure html
+function getProgramFileAddress(html: string, baseAddress: string) {
+  const indVOD = html.indexOf("vod");
+  let str = html.slice(indVOD + 4, html.indexOf('"', indVOD));
+  return baseAddress + str;
+}
+
+function getProgramDetail(html: string) {
+//   const $ = cheerio.load(html);
+//   let itemList = $(".item_div");
+//   console.log(itemList);
+}
 
 async function main() {
-	const response = await axios.get("https://backup.golha.co.uk/fa/programme/18#.Ywb1pX3P3IV");
-		const html = response.data;
-		const indVOD = html.indexOf('vod');
-		let str = html.slice(indVOD+4,html.indexOf('\"',indVOD));
-		console.log("https://backup.golha.co.uk/vod/"+str);
-		
-		const $ = cheerio.load(html,{xmlMode:true});
-	
-		const title = $('h1').contents().first().text();
-		const audioSrc = $('#jp_audio_0');
-		
-		console.log(title);
+  const response = await axios.get(
+    "https://backup.golha.co.uk/fa/programme/1547#.YwrxD0ZByM8"
+  );
+  const html = response.data;
+
+  getProgramDetail(html);
+
+  const puppeteer = require("puppeteer");
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://backup.golha.co.uk/fa/programme/1547#.YwrxD0ZByM8");
+    const text = await page.evaluate('document.querySelector("#jp_audio_0").getAttribute("src")');
+    await browser.close();
+  })();
+  //get file address
+  console.log(getProgramFileAddress(html, "https://backup.golha.co.uk/vod/"));
 }
 
 main();
