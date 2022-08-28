cbad3f3e8e2660b81e79105eed547fb9e8b60754
nrdc-paykanro <112057043+nrdc-paykanro@users.noreply.github.com>
2022-08-28 08:59:44 +0430

    extract program file name

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,16 @@ import cheerio from 'cheerio';
 async function main() {
 	const response = await axios.get("https://backup.golha.co.uk/fa/programme/18#.Ywb1pX3P3IV");
 		const html = response.data;
-
-		const $ = cheerio.load(html);
-
-		const links = $('#jp_audio_0').get();
-		console.log(links);
+		const indVOD = html.indexOf('vod');
+		let str = html.slice(indVOD+4,html.indexOf('\"',indVOD));
+		console.log(str);
+		
+		const $ = cheerio.load(html,{xmlMode:true});
+	
+		const title = $('h1').contents().first().text();
+		const audioSrc = $('#jp_audio_0');
+		
+		console.log(title);
 }
 
 main();
