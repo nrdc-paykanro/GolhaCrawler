79e150cca9f9f80ad37f4ea325a724a9ff1d46f9
nrdc-paykanro <112057043+nrdc-paykanro@users.noreply.github.com>
2022-08-28 16:30:14 +0430

    get element

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,23 @@ async function main() {
   const puppeteer = require("puppeteer");
 
   (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: "headless",
+  });
     const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
     await page.goto("https://backup.golha.co.uk/fa/programme/1547#.YwrxD0ZByM8");
-    const text = await page.evaluate('document.querySelector("#jp_audio_0").getAttribute("src")');
+    //const audioSrc = await page.evaluate('document.querySelector("#jp_audio_0").getAttribute("src")');
+    // const b = await page.evaluate('document.querySelector(".item_div")');
+
+    // const n = await page.$eval(".item_div");
+    let a = await page.evaluate(() => {
+      let data = [];
+      let elements = document.getElementsByClassName('item_div');
+      for (var element of elements)
+          data.push(element.textContent);
+      return data;
+  });
     await browser.close();
   })();
   //get file address
